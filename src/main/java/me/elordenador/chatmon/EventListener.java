e472e722dde0a5a80c688841d52ac9b41a1de1f5
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2022-08-23 13:37:31 +0200

    EventHandler was not declared

--- src/main/java/me/elordenador/chatmon/EventListener.java
+++ src/main/java/me/elordenador/chatmon/EventListener.java
@@ -2,6 +2,7 @@ package me.elordenador.chatmon;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class EventListener implements Listener {
     JavaPlugin plugin;
+    @EventHandler
     public void onMessageSend(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         Utilities utils = new Utilities();

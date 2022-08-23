3d77c0645cd54b40b240d0599ac1f4ff741fd1e0
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2022-08-23 14:45:37 +0200

    Fix Checker

--- src/main/java/me/elordenador/chatmon/Utilities.java
+++ src/main/java/me/elordenador/chatmon/Utilities.java
@@ -7,7 +7,7 @@ public class Utilities {
     public boolean StringPresent(List<String> list, String item) {
         for (int i = 0; i < list.size(); i++) {
             String ite = list.get(i);
-            if (ite.contains(item)) {
+            if (item.contains(ite)) {
                 return true;
             }
         }
